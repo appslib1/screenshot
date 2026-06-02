d97fc817054379ddf920e1129af0a042cb48391f
appsLib1 <jouad.abderrahim7@gmail.com>
2026-06-02 20:37:22 +0100

    floating btn movable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.screenshot_capture.screenshot_photo"
         minSdk = 21
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.12"
+        versionCode = 18
+        versionName = "1.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
